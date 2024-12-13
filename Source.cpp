e4e44424483b760aeae9fac8c154b797f011812d
denischumak <86205264+denischumak@users.noreply.github.com>
2024-12-13 05:20:56 +0500

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -12,30 +12,40 @@ int main() {
 	std::cout << "Vertices" << std::setw(20) << " V ~ E" << '\t'
 		<< std::setw(20) << "V^1.5 ~ E" << '\t'
 		<< std::setw(20) << "V^2 ~ E" << '\n';
-
+	size_t avg_value;
 	for (int i = 2; i <= 10000; i *= 2)
 	{
 		std::cout << i << '\t';
-		auto graph = GraphGenerator::randomGraph_V(i);
-		auto start = std::chrono::system_clock::now();
-		Dijkstra<size_t>::findShortestPaths(graph, 0);
-		auto stop = std::chrono::system_clock::now();
-		auto res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-		std::cout << std::setw(20) << std::to_string(res) + "us" << '\t';
-
-		graph = GraphGenerator::randomGraph_V15(i);
-		start = std::chrono::system_clock::now();
-		Dijkstra<size_t>::findShortestPaths(graph, 0);
-		stop = std::chrono::system_clock::now();
-		res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-		std::cout << std::setw(20) << std::to_string(res) + "us" << '\t';
-
-		graph = GraphGenerator::randomGraph_V2(i);
-		start = std::chrono::system_clock::now();
-		Dijkstra<size_t>::findShortestPaths(graph, 0);
-		stop = std::chrono::system_clock::now();
-		res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
-		std::cout << std::setw(20) << std::to_string(res) + "us" << '\n';
+		avg_value = 0;
+		for (int j = 0; j < 10; ++j) {
+			auto graph = GraphGenerator::randomGraph_V(i);
+			auto start = std::chrono::system_clock::now();
+			Dijkstra<size_t>::findShortestPaths(graph, 0);
+			auto stop = std::chrono::system_clock::now();
+			auto res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+			avg_value += res;
+		}
+		std::cout << std::setw(20) << (double)avg_value / 10 << '\t';
+		avg_value = 0;
+		for (int j = 0; j < 10; ++j) {
+			auto graph = GraphGenerator::randomGraph_V15(i);
+			auto start = std::chrono::system_clock::now();
+			Dijkstra<size_t>::findShortestPaths(graph, 0);
+			auto stop = std::chrono::system_clock::now();
+			auto res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+			avg_value += res;
+		}
+		std::cout << std::setw(20) << (double)avg_value / 10 << '\t';
+		avg_value = 0;
+		for (int j = 0; j < 10; ++j) {
+			auto graph = GraphGenerator::randomGraph_V2(i);
+			auto start = std::chrono::system_clock::now();
+			Dijkstra<size_t>::findShortestPaths(graph, 0);
+			auto stop = std::chrono::system_clock::now();
+			auto res = std::chrono::duration_cast<std::chrono::microseconds>(stop - start).count();
+			avg_value += res;
+		}
+		std::cout << std::setw(20) << (double)avg_value / 10 << '\n';
 	}
 	return 0;
 }
